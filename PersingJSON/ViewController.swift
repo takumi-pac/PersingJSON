f38dc3bf569f1ad7f42b300ee0b1442942e7cd84
takumi-pac <bigbanggd118@gmail.com>
2021-03-06 04:06:58 +0900

    JSON解析の練習

--- PersingJSON/ViewController.swift
+++ PersingJSON/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let jsonURL = Bundle(for: type(of: self)).path(forResource: "example", ofType: "json")else{
+            return
+        }
+        guard let jsonString = try? String(contentsOf: URL(fileURLWithPath: jsonURL), encoding: String.Encoding.utf8)else{
+            return
+        }
+        //jsonString
+        var person: Person?
+        do {
+            person = try JSONDecoder().decode(Person.self, from: Data(jsonString.utf8))
+        } catch  {
+            print(error.localizedDescription)
+        }
+        guard let result = person else {
+            return
+        }
+        print(result.first_name)
+        print(result.last_name)
+        print(result.age)
+        print(result.sublings)
         // Do any additional setup after loading the view.
+    }
+    
+        
     }
 
 
+struct Person: Codable {
+    let first_name:String
+    let last_name:String
+    let age:Int
+    let is_male:Bool
+    let country:String
+    let sublings:[String]
 }
+
+
 
